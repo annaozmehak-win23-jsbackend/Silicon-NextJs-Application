63c1a75b8d92a3b02043a81cc84135a88ea46fac
Anna Özmehak <anna.ozmehak@gmail.com>
2024-05-30 12:40:50 +0200

    subscribe to newsletter done

--- app/components/home/newsletter/Newsletter.tsx
+++ app/components/home/newsletter/Newsletter.tsx
@@ -1,7 +1,49 @@
+'use client'
+
+import { FormEvent, useState } from 'react';
 import styles from './Newsletter.module.css';
 
 export default function Newsletter() {
-  return (
+    const [error, setError] = useState<string>('');
+    const [success, setSuccess] = useState<string>('');
+    const [dailyNewsletter, setDailyNewsletter] = useState(false);
+    const [advertisingUpdates, setAdvertisingUpdates] = useState(false);
+    const [weekInReview, setWeekInReview] = useState(false);
+    const [eventUpdates, setEventUpdates] = useState(false);
+    const [startupsWeekly, setStartupsWeekly] = useState(false);
+    const [podcasts, setPodcasts] = useState(false);
+    const [email, setEmail] = useState('');
+
+    const handleSubscribe = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const data = {
+            email,
+            dailyNewsletter,
+            advertisingUpdates,
+            weekInReview,
+            eventUpdates,
+            startupsWeekly,
+            podcasts
+        }
+
+        const res = fetch('https://newsletterprovider-silicon-win23-annaozmehak.azurewebsites.net/api/Subscribe?code=5CrZZO5rSPxQcqvGKfwv0Zzk0KgWZxytc6rkadfYe9-pAzFuekW0dw%3D%3D', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => {
+            if (res.status === 200) {
+            setSuccess('You are now subscribed'); 
+            } else {
+            setError('Unable to subscribe right now. Try again.');
+            }
+        })
+    }
+
+    return (
         <section id={styles.newsletterSection} className="newsletter-section">
             <div className={`container ${styles.container}`}>
                 <div className={styles.titleBox}>
@@ -9,49 +51,48 @@ export default function Newsletter() {
                     <img src="images/arrows.svg" alt="Arrow" />
                 </div>
 
-                <form id={styles.newsletterForm} method="post" asp-action="Index" asp-controller="Home">
+                <form onSubmit={handleSubscribe} id={styles.newsletterForm} method="post">
                     <div className={styles.checkboxes}>
                         <p className={styles.signUpText}>Sign up for Newsletters</p>
                         <div className={styles.checkboxContainer}>
                             <div>
-                                <input type='checkbox' asp-for="DailyNewsletter" />
-                                <label className={styles.checkboxLabel}>Daily Newsletter</label>
+                                <input type='checkbox' name='dailyNewsletter' onChange={e => setDailyNewsletter(e.target.checked)} />
+                                <label htmlFor='dailyNewsletter' className={styles.checkboxLabel}>Daily Newsletter</label>
                             </div>
                             <div className="checkbox">
-                                <input type='checkbox' asp-for="AdvertisingUpdates" />
-                                <label>Advertising Updates</label>
-                            </div>
-                        </div>
-                        <div className="checkbox-container">
-                            <div className="checkbox">
-                                <input type='checkbox' asp-for="WeekInReview" />
-                                <label>Week In Review</label>
-                            </div>
-                            <div className="checkbox">
-                                <input type='checkbox' asp-for="EventUpdates" />
-                                <label>Event Updates</label>
+                                <input type='checkbox' name='advertisingUpdates' onChange={e => setAdvertisingUpdates(e.target.checked)} />
+                                <label htmlFor='advertisingUpdates'>Advertising Updates</label>
                             </div>
                         </div>
                         <div className="checkbox-container">
                             <div className="checkbox">
-                                <input type='checkbox' asp-for="StartupsWeekly" />
-                                <label>Startups Weekly</label>
+                                <input type='checkbox' name='weekInReview' onChange={e => setWeekInReview(e.target.checked)} />
+                                <label htmlFor='weekInReview'>Week In Review</label>
                             </div>
                             <div className="checkbox">
-                                <input type='checkbox' asp-for="Podcasts" />
-                                <label>Podcasts</label>
+                                <input type='checkbox' name="eventUpdates" onChange={e => setEventUpdates(e.target.checked)} />
+                                <label htmlFor='eventUpdates'>Event Updates</label>
+                            </div>
+                        </div>
+                        <div className="checkbox-container">
+                            <div className="checkbox">
+                                <input type='checkbox' name="startupsWeekly" onChange={e => setStartupsWeekly(e.target.checked)} />
+                                <label htmlFor='startupsWeekly'>Startups Weekly</label>
+                            </div>
+                            <div className="checkbox">
+                                <input type='checkbox' name="podcasts" onChange={e => setPodcasts(e.target.checked)} />
+                                <label htmlFor='podcasts'>Podcasts</label>
                             </div>
                         </div>
                     </div>
 
                     <div className={styles.emailForm}>
-                        <input className={styles.emailInput} asp-for="Email" placeholder='Your Email' />
-                        <span asp-validation-for="Email"></span>
+                        <input className={styles.emailInput} type='email' name='email' placeholder='Your Email' onChange={e => setEmail(e.target.value)} />
                         <button className={`${styles.submitBtn} btn btn-theme`} type="submit">Subscribe*</button>
                     </div>
                     <span className={styles.terms}>* Yes, I agree to the <a href='' className={styles.termsLink}>terms</a> and <a href='' className={styles.termsLink}>privacy policy</a></span>
                 </form>
             </div>
         </section>
-  );
+    );
 }
---
+++ app/interfaces/newsletterTypes.ts
@@ -0,0 +1,9 @@
+export interface Newsletter {
+    email: string;
+    dailyNewsletter: boolean;
+    advertisingUpdates: boolean;
+    weekInReview: boolean;
+    eventUpdates: boolean;
+    startupsWeekly: boolean;
+    podcasts: boolean;
+}
